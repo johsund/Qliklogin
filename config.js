e435fd70c760c62e4b295e1eb0e28138ba40fee7
johsund <jsn@qlik.com>
2016-08-01 15:26:12 +0800

    Final

--- config.js
+++ config.js
@@ -12,19 +12,19 @@ config.port='8084';
 
 //Example RESTURI for API Endpoint. Do not forget to include virtual proxy in path.
 //default: config.RESTURI='https://servername.com:4243/qps/custom';
-config.RESTURI='https://sgsin-jsn1.qliktech.com:4243/qps/custom';
+config.RESTURI='https://qlikserver.domain.com.com:4243/qps/custom';
 
 //If you're using the login page as the "landing page" instead of redirecting from a virtual proxy
 // - make sure you set up this URI to redirect the user after authenticating and receiving a ticket 
-config.REDIRECT='https://sgsin-jsn1.qliktech.com/custom/hub';
+config.REDIRECT='https://qlikserver.domain.com/custom/hub';
 
 //Configure the path for the LDAP server you want to authenticate the user against
-config.LDAP='ldap://qtadsg02.qliktech.com:389/';
+config.LDAP='ldap://ldapserver.domain.com:389/';
 
 //This is appended to the username that the user provides in the form. Example: user keys in 'UserX' and the config.DOMAIN part appends '@domain.com'
-config.DOMAIN = '@qliktech.com';
+config.DOMAIN = '@domain.com';
 
 //Specify the name of the user directory you want to use for the Qlik Sense ticket requests
-config.USERDIRECTORY = 'QTSEL';
+config.USERDIRECTORY = 'USERDIRECTORYNAME';
 
 module.exports = config;
